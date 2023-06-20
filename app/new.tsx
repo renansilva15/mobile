9d7ef557f7ccc05da42e1a1ed64d89a7338eaff6
renansilva15 <renangabrielsilva150@gmail.com>
2023-06-20 10:02:49 -0300

    nlw-spacetime-mobile

---
+++ app/new.tsx
@@ -0,0 +1,165 @@
+import {
+  Image,
+  ScrollView,
+  Switch,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
+
+import NlwSpacetimeLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link, useRouter } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useState } from 'react'
+
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+
+  const [preview, setPreview] = useState<string | null>(null)
+  const [content, setContent] = useState('')
+  const [isPublic, setIsPublic] = useState(false)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        // allowsEditing: true,
+        // aspect: [4, 3],
+        quality: 1,
+      })
+
+      // console.log(result)
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function handleCreateMemory() {
+    try {
+      const token = await SecureStore.getItemAsync('token')
+
+      let coverUrl = ''
+
+      if (preview) {
+        const uploadFormData = new FormData()
+
+        uploadFormData.append('file', {
+          uri: preview,
+          name: 'image.jpg',
+          type: 'image/jpeg',
+        } as any)
+
+        const uploadResponse = await api.post('/upload', uploadFormData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+
+        coverUrl = uploadResponse.data.fileUrl
+
+        // console.log(coverUrl)
+      }
+
+      await api.post(
+        '/memories',
+        {
+          content,
+          isPublic,
+          coverUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+
+      router.push('/memories')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NlwSpacetimeLogo />
+
+        <Link href="/memories" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500">
+            <Icon name="arrow-left" size={16} color="#FFF" />
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+      <View className="mt-6 space-y-6">
+        <View className="flex-row items-center gap-2">
+          <Switch
+            value={isPublic}
+            onValueChange={setIsPublic}
+            trackColor={{ false: '#767577', true: '#372560' }}
+            thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+          />
+
+          <Text className="font-body text-base text-gray-200">
+            Tornar Memória Pública
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={openImagePicker}
+          className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
+        >
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              alt=""
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#FFF">
+                <Text className="font-body text-sm text-gray-200">
+                  Adicionar foto ou vídeo de capa
+                </Text>
+              </Icon>
+            </View>
+          )}
+        </TouchableOpacity>
+
+        <TextInput
+          multiline
+          value={content}
+          onChangeText={setContent}
+          textAlignVertical="top"
+          className="p-0 font-body text-lg text-gray-50"
+          placeholderTextColor="#56565a"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={handleCreateMemory}
+          className="items-center self-end rounded-full bg-green-500 px-5 py-3"
+        >
+          <Text className="font-alt uppercase text-black ">Salvar</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  )
+}
